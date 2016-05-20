8462756a9ccfd5ded0e4c12893f16193ddebd2f2
robhurring <robhurring@gmail.com>
2016-05-20 18:59:49 -0400

    + pocketvim install

---
+++ pocketvim.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+INSTALLPATH="$HOME/.pocket-vimrc"
+\curl -sSL https://raw.githubusercontent.com/robhurring/dotvim/master/pocket-vimrc > $INSTALLPATH
+tee <<"DONE"
+                    _        _             _
+   _ __   ___   ___| | _____| |_    __   _(_)_ __ ___
+  | '_ \ / _ \ / __| |/ / _ \ __|___\ \ / / | '_ ` _ \
+  | |_) | (_) | (__|   <  __/ ||_____\ V /| | | | | | |
+  | .__/ \___/ \___|_|\_\___|\__|     \_/ |_|_| |_| |_|
+  |_|  a light-weight, portable and cozy vimrc!
+
+  Installation complete!
+
+  To use add the following alias to your $HOME/.bashrc:
+
+    alias pvim="vim -u ~/.pocket-vimrc"
+
+  Or run manually with:
+
+    vim -u ~/.pocket-vimrc
+
+  NOTE: Plugins will be installed on first run. You can close
+        the installation window with 'q'
+
+DONE
