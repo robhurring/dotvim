5788945a1ab228bb32da07ca47f0af73437abafc
Rob Hurring <rhurring@salesforce.com>
2024-03-05 10:45:43 -0500

    update lsp settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -32,7 +32,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver' },
+  ensure_installed = { 'tsserver', 'lua_ls', 'gopls', 'jedi_language_server' },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
