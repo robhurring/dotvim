1320fb15391953bb884c5ab9852f370f003d9056
Rob Hurring <rhurring@salesforce.com>
2024-04-08 09:34:42 -0400

    fix: bump lazy lock

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,12 +2,13 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<c-a>", function() harpoon:list():append() end)
+vim.keymap.set("n", "<c-m>", function() harpoon:list():add() end)
+vim.keymap.set("n", "<c-m>", function() harpoon:list():add() end)
 vim.keymap.set("n", "<c-s-a>", function() harpoon:list():remove() end)
-vim.keymap.set("n", "<localleader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<localleader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<localleader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<localleader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
 
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
@@ -26,4 +27,4 @@ local function toggle_telescope(harpoon_files)
   }):find()
 end
 
-vim.keymap.set("n", "<c-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
+vim.keymap.set("n", "<c-h>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
