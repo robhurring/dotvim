29d29efd260c79ae704ad13af7794afb51c63d78
Rob Hurring <rhurring@salesforce.com>
2024-04-16 14:24:55 -0400

    Squashed commit of the following:
    commit 15bae42ae3a1cd50bb97315129b694699816168e
    Author: Rob Hurring <rhurring@salesforce.com>
    Date:   Tue Apr 16 14:24:28 2024 -0400
    
        fix: update lock
    
    commit 2de511ae61c144abf2d415cfb326f8532dceef12
    Author: robhurring <robhurring@gmail.com>
    Date:   Mon Apr 15 21:31:35 2024 -0400
    
        refactor: config hacks

--- nvim/lua/local/pilot.lua
+++ nvim/lua/local/pilot.lua
@@ -3,6 +3,7 @@ local copilot = require('copilot.suggestion')
 local default_config = {
   -- copilot-first experience vs cmp-first experience
   enabled = false
+  --@todo move the crazy S/TAB cmp mappings here to avoid conflicts
 }
 
 local mod = {
@@ -15,15 +16,26 @@ end
 
 function mod.toggle()
   mod.config.enabled = not mod.config.enabled
-  --@todo find a better way to toggle, this assumes it always starts as off
   copilot.toggle_auto_trigger()
   print("pilot is now " .. (mod.config.enabled and "enabled" or "disabled"))
 end
 
---@todo add setup here with the actual copilot plugin and cmp plugins
 function mod.setup(opts)
   local config = vim.tbl_deep_extend("force", default_config, opts or {})
   mod.config = config
+
+  local copilot_config = {
+    suggestion = {
+      auto_trigger = config.enabled,
+      keymap = {
+        -- accept = "<tab>"
+      }
+    }
+  }
+
+  require("copilot").setup(copilot_config)
+
+  return mod
 end
 
 return mod
